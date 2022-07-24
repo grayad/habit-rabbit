f1e5ac789b04d6b6286f1ef49281a8f51f7d0d32
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-07-24 19:57:05 -0400

    refined and created user api routes

--- routes/api-routes/user-routes.js
+++ routes/api-routes/user-routes.js
@@ -65,4 +65,43 @@ router.post('/login', (req, res) => {
     });
 });
 
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;

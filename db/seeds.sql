31d2b4880a7acc44c9cb76e1303a11e160c9d8cf
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-07-23 20:38:00 -0400

    created initial habit and counts models

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use habit_rabbit_db;
+
+INSERT INTO user (email, password)
+VALUES
+    ('ron@gmail.com', 'pass1'),
+    ('mac@gmail.com', 'pass2'),
+    ('tina@gmail.com', 'pass3');
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
+const User = require('./Habit');
+const User = require('./Counts.js');
 
-module.exports = { User };
+module.exports = { User, Habit, Counts };

417ecfdafcb49c304315d422d443eb757183ad44
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-24 20:49:42 -0400

    thought process for dynamic html

---
+++ public/assets/js/index.js
@@ -0,0 +1,54 @@
+// thoughts:
+// query to get all habits and counts (array of objects)
+// then forEach object in the array, run the generateCard function OR use JS to create 
+// each element with the appropriate classes
+
+const getHabits = () =>
+  fetch('/api/habits', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+});
+
+const addHabit = (habit) =>
+  fetch('/api/habits', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(habit),
+});
+
+const deleteHabit = (id) =>
+  fetch(`/api/habits/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+});
+
+const generateCard = habit => {
+    return `
+    <div class="card">
+                <div class="card-container">
+                    <h4>${habit.name}</h4>
+                    <div class="align">
+                        <label for="habitType">Habit Type:</label>
+                        <label for="habitType" type="text" id="habitType" >${habit.type}</label>
+                    </div>
+
+                    <div class="align">
+                        <label for="targetDays">Target #:</label>
+                        <label for="targetDays" type="text" id="targetDays" >${habit.targetDays}</label>
+                    </div>
+
+                    <div class="align">
+                        <label for="daysCompleted">Days Completed:</label>
+                        <label for="daysCompleted" type="text" id="daysCompleted" >${habit.count}</label>
+                    </div>
+                </div>
+                <button class="addbutton"><span class="plus">+</span></button>
+            </div>
+        `
+}

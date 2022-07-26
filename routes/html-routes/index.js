e9f97022a509beaf7a9be95ac5f48955aa561263
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-25 20:04:10 -0400

    fix createhabits.html css hrefs

--- routes/html-routes/index.js
+++ routes/html-routes/index.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
 
-
 // Homepage
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/home.html'))

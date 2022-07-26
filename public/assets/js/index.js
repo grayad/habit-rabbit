50f067607d3e04d9ce40bc9448dd149111425817
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-25 21:14:01 -0400

    on submit, package form inputs as object

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,13 +1,43 @@
 
-if (window.location.pathname === '/create-habit') {
+
 var habitInputEl = document.getElementById('habitName');
 var typeInputEl = document.getElementById('habitType');
 var daysInputEl = document.getElementById('targetDays');
 var formBtnEl = document.getElementById('createHabit');
-}
+
+
+// const addHabit = (habit) =>
+//     fetch('/api/habits', {
+//         method: 'POST',
+//         headers: {
+//         'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(habit),
+//     });
 
 var formSubmitHandler = function() {
-    console.log(">>>button clicked")
+    console.log(">>>button clicked");
+
+    // get values from inputs and trim any leading/trailing spaces
+    var habitName=habitInputEl.value.trim();
+    var habitType=typeInputEl.value.trim();
+    var habitDays=daysInputEl.value.trim();
+
+    if(habitName, habitType, habitDays) {
+        const newHabit = {
+            name: habitName,
+            type: habitType,
+            targetDays: habitDays
+        };
+        console.log(newHabit);
+    } else {
+        // if any inputs left blank
+        alert("Please complete all fields.")
+    }
+
+    // addHabit(newHabit).then(() => {
+
+    // })
 }
 
-formBtnEl.addEventListener("submit", formSubmitHandler);
+formBtnEl.addEventListener("click", formSubmitHandler);

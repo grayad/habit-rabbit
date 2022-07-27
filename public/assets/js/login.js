c33756e4ab0dc43c5c34b948d945389b85380a74
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-27 18:29:05 -0400

    get user inputs and create new user

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -5,12 +5,43 @@ if (window.location.pathname === "/") {
   var createAcctBtn = document.getElementById("createAccount");
 }
 
+// function to post new user to db on form submit
+const addUser = (user) =>
+  fetch("/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+
 var loginHandler = function () {
   console.log(">>>login button clicked");
 };
 
 var createAcctHandler = function () {
   console.log(">>>create account button clicked");
+
+  // get values from inputs and trim any leading/trailing spaces
+  var userEmail = emailInputEl.value.trim();
+  var userPW = passwordInputEl.value.trim();
+
+  let newUser;
+
+  if ((userEmail, userPW)) {
+    newUser = {
+      email: userEmail,
+      password: userPW,
+    };
+    console.log(newUser);
+  } else {
+    // if any inputs left blank
+    alert("Please complete all fields.");
+  }
+
+  addUser(newUser).then(() => {
+    alert("Account created!.");
+  });
 };
 
 createAcctBtn.addEventListener("click", createAcctHandler);

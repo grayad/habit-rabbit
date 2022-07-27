232f662fbad072b5cad91112f468c91a2ffbc2b1
hlnicks <hlnicks@icloud.com>
2022-07-27 19:00:29 -0400

    worked on alignment/general HTML

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'mysql',
     port: 3306
   });

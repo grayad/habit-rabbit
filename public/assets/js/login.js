f6f86476a3f1591c93b80a51d054a815fa7b3f05
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-07-27 18:44:00 -0400

    refactor createAccHandler function

--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -5,43 +5,35 @@ if (window.location.pathname === "/") {
   var createAcctBtn = document.getElementById("createAccount");
 }
 
-// function to post new user to db on form submit
-const addUser = (user) =>
-  fetch("/api/users", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(user),
-  });
-
 var loginHandler = function () {
   console.log(">>>login button clicked");
 };
 
-var createAcctHandler = function () {
+var createAcctHandler = async function (event) {
   console.log(">>>create account button clicked");
+
+  event.preventDefault();
 
   // get values from inputs and trim any leading/trailing spaces
   var userEmail = emailInputEl.value.trim();
   var userPW = passwordInputEl.value.trim();
 
-  let newUser;
-
-  if ((userEmail, userPW)) {
-    newUser = {
+  const response = await fetch("/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
       email: userEmail,
       password: userPW,
-    };
-    console.log(newUser);
-  } else {
-    // if any inputs left blank
-    alert("Please complete all fields.");
-  }
-
-  addUser(newUser).then(() => {
-    document.location.replace("/home");
+    }),
   });
+
+  if (response.ok) {
+    document.location.replace("/home");
+  } else {
+    alert("Failed to sign up");
+  }
 };
 
 createAcctBtn.addEventListener("click", createAcctHandler);

d860c5d7399899bfafc44263a5cb41f25d1867e6
hlnicks <hlnicks@icloud.com>
2022-07-31 16:06:56 -0400

    edited css

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
